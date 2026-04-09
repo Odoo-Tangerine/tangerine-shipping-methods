b812a7ab60fab6ab23c838a88f213045a062388d
Tangerine <odoo.tangerine@gmail.com>
2026-04-09 22:40:16 +0700

    [UDP] - Open source

--- tangerine_delivery_grab/__manifest__.py
+++ tangerine_delivery_grab/__manifest__.py
@@ -25,6 +25,4 @@
     'installable': True,
     'auto_install': False,
     'application': True,
-    'currency': 'USD',
-    'price': 119.00
 }
--- tangerine_delivery_viettelpost/__manifest__.py
+++ tangerine_delivery_viettelpost/__manifest__.py
@@ -28,6 +28,4 @@
     'installable': True,
     'auto_install': False,
     'application': True,
-    'currency': 'USD',
-    'price': 119.00
 }
